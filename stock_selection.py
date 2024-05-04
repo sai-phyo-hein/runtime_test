e99d04d3e864dced6e8c8f8601804076b64c2e44
sai_phyo_hein <63901802+sai-phyo-hein@users.noreply.github.com>
2024-05-04 09:34:10 +0000

    update_correlation_criterion

--- stock_selection.py
+++ stock_selection.py
@@ -7,8 +7,6 @@ import json
 config = json.load(open('/workspaces/runtime_test/data_config.json', 'r'))
 nf_50_tickers_df = pd.read_csv('/workspaces/runtime_test/nifty_50_metadata.csv')[['Industry', 'Symbol']]
 nf_50_tickers_df.Symbol = nf_50_tickers_df.Symbol + '.NS'
-nf_50_tickers_df = nf_50_tickers_df.groupby('Industry').head(3)
-(nf_50_tickers_df.shape)
 nf_50_tickers = nf_50_tickers_df.Symbol.tolist() 
 
 data = yf.download(
@@ -18,7 +16,7 @@ data = yf.download(
 )['Adj Close'].dropna(axis = 1)
 
 corr = data.corr() 
-corr_lt_05 = corr[corr < 0.5]
+corr_lt_05 = corr[corr < 0.45]
 corr_lt_05.dropna(thresh = 6, axis = 0, inplace = True)
 corr_lt_05.dropna(thresh = 6, axis = 1, inplace = True)
 print(corr_lt_05.shape)
