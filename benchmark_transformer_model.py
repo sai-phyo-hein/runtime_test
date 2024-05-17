a56cd296cc866366e6e4a00a047428726882e959
sai_phyo_hein <63901802+sai-phyo-hein@users.noreply.github.com>
2024-05-17 11:05:09 +0630

    Update benchmark_transformer_model.py - loss function

--- benchmark_transformer_model.py
+++ benchmark_transformer_model.py
@@ -101,11 +101,12 @@ class Transformer_Portfolio(tf.keras.layers.Layer):
 
         #Configuring Custom Loss Funciton with Mean Sharpe Ratio
         def sharpe_loss(_, y_pred):
-            data = self.priceData
+            data = tf.divide(self.priceData, self.priceData[0])
             y_pred = tf.unstack(y_pred)
             sharpes = tf.zeros((1,1))
             for y in y_pred:
-                portfolio_returns = tf.reduce_sum(tf.multiply(data, y), axis=1, ) 
+                portfolio_values = tf.reduce_sum(tf.multiply(data, y), axis=1, )
+                portfolio_returns = (portfolio_values[1:] - portfolio_values[:-1]) / portfolio_values[:-1]  # % change formula
                 sharpe = tf.keras.backend.mean(portfolio_returns) / tf.keras.backend.std(portfolio_returns)
                 sharpes = tf.concat((sharpes, tf.reshape(sharpe, (1, -1))), axis = 0)
             return -tf.keras.backend.mean(sharpes[0][1:])
